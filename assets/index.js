da1ad5e3bd1b186290bc02add9188389e2b8b162
Zackry Langford <104527780+zackrylangford@users.noreply.github.com>
2024-01-20 03:02:50 +0000

    Update company name and styling

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 const currentYear = new Date().getFullYear();
-const companyName = 'Sweets & Treats';
+const companyName = 'Heats and Sweets';
 
 const copyrightInfo = `&copy; ${currentYear} ${companyName}. All rights reserved.`;
 
